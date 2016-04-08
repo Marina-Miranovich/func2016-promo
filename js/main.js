d739ed28be06c39144668874bbb583198c8b6044
Obnoxious Mari <miranovichmarina@gmail.com>
2016-04-07 18:26:29 -0700

    mobile menu

--- js/main.js
+++ js/main.js
@@ -1,11 +1,33 @@
 (function (w) {
+	var OPEN_MENU_CLASS = 'menu-opened';
+	var MIN_DESCTOP_WIDTH = 770;
+	var DEBOUNCE_DELAY = 100;
 	var topNav = w.document.getElementById('topNav');
+	var debounceTimer;
 
 	topNav.addEventListener('click', function () {
 		toggleMenu();
 	});
 
+	w.addEventListener('resize', function () {
+		if (debounceTimer) {
+			w.clearTimeout(debounceTimer);
+		}
+		w.setTimeout(function () {
+			if (w.innerWidth >= MIN_DESCTOP_WIDTH) {
+				closeMenu();
+			}
+		}, DEBOUNCE_DELAY);
+
+	});
+
 	function toggleMenu() {
-		
+		if (w.innerWidth < MIN_DESCTOP_WIDTH) {
+			topNav.classList.toggle(OPEN_MENU_CLASS);
+		}
+	}
+
+	function closeMenu () {
+		topNav.classList.remove(OPEN_MENU_CLASS);
 	}
 }) (window);
